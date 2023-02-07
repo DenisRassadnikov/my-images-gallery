88d4b1f0f4574d23281254d508bcfbd6fede038b
DenisRassadnikov <newdrgit@gmail.com>
2023-02-07 11:31:30 +0200

    fixed code with lint

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,19 @@
-import React from "react"
-import { Navbar, Container } from "react-bootstrap"
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
-  backgroundColor: "#343A40",
-}
+  backgroundColor: '#343A40',
+};
 
 const Header = (props) => {
-  const { title } = props
+  const { title } = props;
   return (
-    <Navbar style={navbarStyle} variant='dark'>
+    <Navbar style={navbarStyle} variant="dark">
       <Container>
-        <Navbar.Brand href='/'>{title}</Navbar.Brand>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
